832c25e9a748f4df65757a05f6c749971dbea9d4
jadynai <936745006@qq.com>
2018-06-21 14:52:11 +0800

    DSL适配器优化;新增测试代码

--- app/src/main/java/com/motong/cm/kotlintest/MainActivity.kt
+++ app/src/main/java/com/motong/cm/kotlintest/MainActivity.kt
@@ -1,61 +1,73 @@
 package com.motong.cm.kotlintest
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
 import android.view.View
-import com.motong.cm.kotlintest.rdsll.AcrobatAdapter
 import com.motong.cm.kotlintest.rdsll.AcrobatItem
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_test.view.*
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.button
+import org.jetbrains.anko.verticalLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        setContentView(R.layout.activity_main)
-
-        val l = ArrayList<Int>()
-        for (i in 0 until 500) {
-            l.add(i)
-        }
-
-        val acrobatAdapter = AcrobatAdapter<Int> {
-
-            itemDSL {
-                resId(R.layout.item_test)
-                showItem { d, pos, view ->
-                    view.item_tv.text = "ttt: " + d
-                }
-                isMeetData { d, pos ->
-                    pos == 1
+        verticalLayout { 
+            button("hahha"){
+                setOnClickListener { 
+                    askDialog(this@MainActivity,"this is title"){
+                        okButton { 
+                            toastS(this@MainActivity,"toast ")
+                        }
+                        
+                    }.show()
+                    
                 }
             }
-
-            itemDSL {
-                resId(R.layout.item_test1)
-                showItem { d, pos, view ->
-                    view.item_tv.text = "cece: " + d
-                }
-                isMeetData { d, pos -> pos != 1 }
-            }
-
-        }.bindEvent {
-            onClick {
-                toast("test" + adapterPosition)
-            }
         }
-        recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.adapter = acrobatAdapter
 
-        acrobatAdapter.setDataWithDiff(l)
-
-        change_tv.setOnClickListener {
-            acrobatAdapter.setDataWithDiff(arrayListOf(1, 2, 3, 4, 5, 5, 44, 444, 55))
-        }
+        val builder = AlertDialog.Builder(this)
+//        setContentView(R.layout.activity_main)
+//
+//        val l = ArrayList<Int>()
+//        for (i in 0 until 500) {
+//            l.add(i)
+//        }
+//
+//        val acrobatAdapter = AcrobatAdapter<Int> {
+//
+//            itemDSL {
+//                resId(R.layout.item_test)
+//                showItem { d, pos, view ->
+//                    view.item_tv.text = "ttt: " + d
+//                }
+//                isMeetData { d, pos ->
+//                    pos == 1
+//                }
+//            }
+//
+//            itemDSL {
+//                resId(R.layout.item_test1)
+//                showItem { d, pos, view ->
+//                    view.item_tv.text = "cece: " + d
+//                }
+//                isMeetData { d, pos -> pos != 1 }
+//            }
+//
+//        }.bindEvent {
+//            onClick {
+//                toast("test" + adapterPosition)
+//            }
+//        }
+//        recycler_view.layoutManager = LinearLayoutManager(this)
+//        recycler_view.adapter = acrobatAdapter
+//
+//        acrobatAdapter.setDataWithDiff(l)
+//
+//        change_tv.setOnClickListener {
+//            acrobatAdapter.setDataWithDiff(arrayListOf(1, 2, 3, 4, 5, 5, 44, 444, 55))
+//        }
     }
 }
 
