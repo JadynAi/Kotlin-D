d884cfce83ef457151502a6e8ef493e50b05a1e0
AiLoli <aa>
2018-06-23 23:37:35 +0800

    代码格式化

--- app/src/main/java/com/motong/cm/kotlintest/BaseFragment.kt
+++ app/src/main/java/com/motong/cm/kotlintest/BaseFragment.kt
@@ -2,33 +2,19 @@ package com.motong.cm.kotlintest
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 abstract class BaseFragment : Fragment(){
-
-    protected var TAG = this.javaClass.name
-
     lateinit var mRootView: View
-
     private var isVisibleToUsers = false
     private var isOnCreateView = false
     private var isSetUserVisibleHint = false
-    private var isByStop = false
-    private var isPostSetVisibleTask = false
     private var isHiddenChanged = false
-    private val setVisibleTask = Runnable {
-        isPostSetVisibleTask = false
-        setVisibleToUser(true)
-    }
-
-    val pageName: String
-        get() = javaClass.simpleName
-
+    private var isFirstResume = false
+    
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Log.d(TAG, "onCreateView : ")
         isOnCreateView = true
         mRootView = LayoutInflater.from(activity).inflate(getResId(), null, false)
         return mRootView
@@ -36,60 +22,43 @@ abstract class BaseFragment : Fragment(){
 
     abstract fun getResId(): Int
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "onViewCreated : ")
-    }
-
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "onResume : ")
-
-        if (!isSetUserVisibleHint && !isHiddenChanged) {
-            setVisibleToUser(true)
-        } else if (isByStop) {
-            isPostSetVisibleTask = true
+        if (!isHiddenChanged && !isSetUserVisibleHint) {
+            if (isFirstResume) {
+                setVisibleToUser(true)
+            }
         }
-        isByStop = false
+        if (isSetUserVisibleHint || (!isFirstResume && !isHiddenChanged)) {
+            isVisibleToUsers = true
+        }
+        isFirstResume = true
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAG, "onPause : ")
         isHiddenChanged = false
-        if (isVisibleToUsers) {
-            isByStop = true
-        }
+        isSetUserVisibleHint = false
         setVisibleToUser(false)
     }
-
+    
     override fun setUserVisibleHint(isVisibleToUser: Boolean) {
         super.setUserVisibleHint(isVisibleToUser)
-        Log.d(TAG, "setUserVisibleHint")
-        removeSetVisibleTask()
         isSetUserVisibleHint = true
         setVisibleToUser(isVisibleToUser)
     }
 
     override fun onHiddenChanged(hidden: Boolean) {
         super.onHiddenChanged(hidden)
-        Log.d(TAG, "onHiddenChanged : " + hidden)
-        removeSetVisibleTask()
         isHiddenChanged = true
         setVisibleToUser(!hidden)
     }
-
-    private fun removeSetVisibleTask() {
-        if (isPostSetVisibleTask) {
-            isPostSetVisibleTask = false
-        }
-    }
-
+    
     private fun setVisibleToUser(isVisibleToUser: Boolean) {
         if (!isOnCreateView) {
             return
         }
-        if (isVisibleToUser == isVisibleToUsers && !isHiddenChanged) {
+        if (isVisibleToUser == isVisibleToUsers) {
             return
         }
         isVisibleToUsers = isVisibleToUser
@@ -97,6 +66,5 @@ abstract class BaseFragment : Fragment(){
     }
 
     protected open fun onVisibleToUserChanged(isVisibleToUser: Boolean) {
-        Log.d(TAG, "##onVisibleToUserChanged() isVisibleToUser = " + isVisibleToUser)
     }
 }
