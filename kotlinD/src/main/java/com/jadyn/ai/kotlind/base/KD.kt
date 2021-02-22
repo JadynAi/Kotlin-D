c1df7871a76a0b14700c53e843f34ca026e0ffa9
JadynAi <936745006@qq.com>
2021-02-22 14:59:04 +0800

    调试KDUI初始化问题

--- kotlinD/src/main/java/com/jadyn/ai/kotlind/base/KD.kt
+++ kotlinD/src/main/java/com/jadyn/ai/kotlind/base/KD.kt
@@ -1,6 +1,14 @@
 package com.jadyn.ai.kotlind.base
 
+import android.app.Activity
 import android.app.Application
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import androidx.core.view.LayoutInflaterCompat
 
 /**
  *@version:
@@ -17,9 +25,41 @@ class KD {
         fun init(a: Application) {
             if (!::application.isInitialized) {
                 application = a
+                initUI(a)
             }
         }
 
         fun applicationWrapper() = application
+
+        private fun initUI(app: Application) {
+            app.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+                override fun onActivityPaused(activity: Activity) {}
+                override fun onActivityStarted(activity: Activity) {}
+                override fun onActivityDestroyed(activity: Activity) {}
+                override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+                override fun onActivityStopped(activity: Activity) {}
+                override fun onActivityResumed(activity: Activity) {}
+                override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                    registerKUI(activity, savedInstanceState)
+                }
+            })
+        }
+
+        private fun registerKUI(activity: Activity, savedInstanceState: Bundle?) {
+            val forceSetFactory2 = LayoutInflaterCompat::class.java.getDeclaredMethod("forceSetFactory2", LayoutInflater::class.java, LayoutInflater.Factory2::class.java)
+            val lf = LayoutInflater.from(activity)
+            forceSetFactory2.isAccessible = true
+            forceSetFactory2.invoke(null, lf, object : LayoutInflater.Factory2 {
+                override fun onCreateView(parent: View?, name: String, context: Context, attrs: AttributeSet): View? {
+                    Log.d("KD", "onCreateView: parent view")
+                    return activity.onCreateView(parent, name, context, attrs)
+                }
+
+                override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+                    Log.d("KD", "onCreateView: name string")
+                    return activity.onCreateView(name, context, attrs)
+                }
+            })
+        }
     }
 } 
---
+++ kotlinU/src/main/java/com/jadyn/kotlinu/KUI.kt
@@ -0,0 +1,10 @@
+package com.jadyn.kotlinu
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import android.view.LayoutInflater
+
+/**
+ *JadynAi since 2021/2/22
+ */
