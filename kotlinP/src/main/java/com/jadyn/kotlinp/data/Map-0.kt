7424fc5bdbd64dc004f897c77dc4e33756e94a03
JadynAi <936745006@qq.com>
2021-03-10 17:41:54 +0800

    增加数据结构测试类

---
+++ kotlinP/src/main/java/com/jadyn/kotlinp/data/Map-0.kt
@@ -0,0 +1,11 @@
+package com.jadyn.kotlinp.data
+
+import java.util.HashMap
+
+/**
+ *JadynAi since 2021/3/10
+ */
+fun main() {
+    HashMap<String,String>(1,0.2f)
+    print("result ${map0JavaTest.test1()}")
+}
---
+++ kotlinP/src/main/java/com/jadyn/kotlinp/data/map0JavaTest.java
@@ -0,0 +1,11 @@
+package com.jadyn.kotlinp.data;
+
+/**
+ * JadynAi since 2021/3/10
+ */
+class map0JavaTest {
+
+    public static int test1() {
+        return (1 << 30); 
+    }
+}
