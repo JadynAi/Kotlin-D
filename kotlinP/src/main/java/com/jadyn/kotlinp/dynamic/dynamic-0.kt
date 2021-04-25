fefb1d406b434562256ffd5737395d29e254683d
JadynAI <936745006@qq.com>
2021-04-25 23:19:22 +0800

    增加动态规划测试代码[2]:爬楼梯和最大子序列

--- kotlinP/src/main/java/com/jadyn/kotlinp/dynamic/dynamic-0.kt
+++ kotlinP/src/main/java/com/jadyn/kotlinp/dynamic/dynamic-0.kt
@@ -1,10 +1,13 @@
 package com.jadyn.kotlinp.dynamic
 
+import kotlin.math.max
+
 /**
  *JadynAi since 4/20/21
  */
 fun main() {
-    println("fib result ${fib(4)}")
+//    println("fib result ${maxSubArray(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4))}")
+    println("counts ${climbStairs(4)}")
 }
 
 /**
@@ -22,6 +25,19 @@ fun fib(n: Int): Int {
     return cur
 }
 
+/**
+ * 最大子序和
+ * */
+fun maxSubArray(nums: IntArray): Int {
+    var sum = 0
+    var maxAnswer = nums[0]
+    for (num in nums) {
+        sum = max(sum + num, num)
+        maxAnswer = max(sum, maxAnswer)
+    }
+    return maxAnswer
+}
+
 fun coinChange(coins: IntArray, amount: Int): Int {
     if (amount == 0) return 0
     if (amount < 0) return -1
@@ -32,4 +48,20 @@ fun coinChange(coins: IntArray, amount: Int): Int {
         }
     }
     return -1
+}
+
+/**
+ * 爬n楼梯，每次只能爬1或者2，共有多少种解法
+ * */
+fun climbStairs(n: Int): Int {
+    if (n <= 0) {
+        return 0
+    }
+    val countsArray = arrayOfNulls<Int>(n)
+    countsArray[0] = 1
+    countsArray[1] = 2
+    for (i in 2 until n) {
+        countsArray[i] = countsArray[i - 1]!! + countsArray[i - 2]!!
+    }
+    return countsArray[n - 1]!!
 }
