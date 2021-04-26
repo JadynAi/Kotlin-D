c3e9e18b431ad23f1e80392bd03fd2e0b1aa7ee1
JadynAI <936745006@qq.com>
2021-04-26 22:15:26 +0800

    增加动态规划测试代码[3]:股票买卖

--- kotlinP/src/main/java/com/jadyn/kotlinp/dynamic/dynamic-0.kt
+++ kotlinP/src/main/java/com/jadyn/kotlinp/dynamic/dynamic-0.kt
@@ -7,7 +7,8 @@ import kotlin.math.max
  */
 fun main() {
 //    println("fib result ${maxSubArray(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4))}")
-    println("counts ${climbStairs(4)}")
+//    println("counts ${climbStairs(4)}")
+    println("max profit ${maxProfit1(intArrayOf(3, 3, 6, 7, 1, 6, 5, 4, 10))}")
 }
 
 /**
@@ -64,4 +65,38 @@ fun climbStairs(n: Int): Int {
         countsArray[i] = countsArray[i - 1]!! + countsArray[i - 2]!!
     }
     return countsArray[n - 1]!!
+}
+
+/**
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。
+ * 设计一个算法来计算你所能获取的最大利润。
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0
+ * */
+fun maxProfit(prices: IntArray): Int {
+    if (prices.size == 0) return 0
+    var max = 0
+    var min = prices[0]
+    for (i in 0..prices.lastIndex) {
+        if (prices[i] <= min) {
+            min = prices[i]
+        } else if (prices[i] - min > max) {
+            max = prices[i] - min
+        }
+    }
+    return if (max < 0) 0 else max
+}
+
+fun maxProfit1(prices: IntArray): Int {
+    if (prices.size == 0) return 0
+    var max = 0
+    // 4/26/21-10:05 PM 前一天的利润
+    var pre = 0
+    for (i in 1..prices.lastIndex) {
+        val diff = prices[i] - prices[i - 1]
+        // 4/26/21-10:04 PM 状态转移方程，i天的利润就是前一天的利润，加上i天的i-1的利润差
+        pre = Math.max(0, pre + diff)
+        max = Math.max(max, pre)
+    }
+    return max
 }
