3fc9fc5b3834860cc1a0d93b1b4bb7f08646e87a
JadynAI <936745006@qq.com>
2021-06-29 23:11:30 +0800

    移除无用webSocket测试类

--- app/src/main/java/com/jadynai/kotlindiary/websocket/TraceServerMgr.java
+++ app/src/main/java/com/jadynai/kotlindiary/websocket/TraceServerMgr.java
@@ -1,27 +1,27 @@
-package com.jadynai.kotlindiary.websocket;
-
-import android.content.Context;
-
-/**
- * JadynAi since 2020/9/15
- */
-class TraceServerMgr {
-
-    private TServer server;
-    
-    private TraceServerMgr() {
-        server = new TServer();
-    }
-
-    public void startServer(Context context) {
-        server.start(context);
-    }
-
-    public static TraceServerMgr getInstance() {
-        return Holder.sInstance;
-    }
-
-    public static class Holder {
-        public static TraceServerMgr sInstance = new TraceServerMgr();
-    }
-}
+//package com.jadynai.kotlindiary.websocket;
+//
+//import android.content.Context;
+//
+///**
+// * JadynAi since 2020/9/15
+// */
+//class TraceServerMgr {
+//
+//    private TServer server;
+//    
+//    private TraceServerMgr() {
+//        server = new TServer();
+//    }
+//
+//    public void startServer(Context context) {
+//        server.start(context);
+//    }
+//
+//    public static TraceServerMgr getInstance() {
+//        return Holder.sInstance;
+//    }
+//
+//    public static class Holder {
+//        public static TraceServerMgr sInstance = new TraceServerMgr();
+//    }
+//}
